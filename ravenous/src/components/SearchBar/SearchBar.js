87fe302c024e0a2f7552c5bb7ccbc6c92d8f0e0b
sli41 <sli41@ncsu.edu>
2019-05-20 21:35:53 -0700

    Add event handlers to SearchBar for options and inputs

--- ravenous/src/components/SearchBar/SearchBar.js
+++ ravenous/src/components/SearchBar/SearchBar.js
@@ -2,20 +2,19 @@
 import React from 'react';
 import './SearchBar.css';
 
-const sortByOptions = {
-  "Best Match": "best_match",
-  "Highest Rated": "rating",
-  "Most Reviewed": "review_count"
-};
-
 class SearchBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      "term": '',
-      "location": '',
-      "sortBy": 'best_match'
+      term: '',
+      location: '',
+      sortBy: 'best_match'
     };
+
+    this.handleSortByChange = this.handleSortByChange.bind(this);
+    this.handleTermChange = this.handleTermChange.bind(this);
+    this.handleLocationChange = this.handleLocationChange.bind(this);
+
     this.sortByOptions = {
       "Best Match": "best_match",
       "Highest Rated": "rating",
@@ -27,11 +26,28 @@ class SearchBar extends React.Component {
     return this.sortBy === sortByOption ? 'active' : '';
   }
 
+  handleSortByChange(sortByOption) {
+    this.setState({"sortBy": sortByOption});
+  }
+
+  handleTermChange(event) {
+    this.setState({term: event.target.value});
+  }
+
+  handleLocationChange(event) {
+    this.setState({location: event.target.value});
+  }
+
   renderSortByOptions() {
-    return Object.keys(sortByOptions).map(
+    return Object.keys(this.sortByOptions).map(
       sortByOption => {
-        let sortByOptionValue = sortByOptions[sortByOption];
-        return <li key={sortByOptionValue}>{sortByOption}</li>;
+        let sortByOptionValue = this.sortByOptions[sortByOption];
+        return <li 
+            key={sortByOptionValue} 
+            className={this.getSortByClass(sortByOptionValue)} 
+            onClick={this.handleSortByChange.bind(this, sortByOptionValue)}>
+              {sortByOption}
+          </li>;
       }
     );
   }
@@ -45,8 +61,8 @@ class SearchBar extends React.Component {
           </ul>
         </div>
         <div className="SearchBar-fields">
-          <input placeholder="Search Businesses" />
-          <input placeholder="Where?" />
+          <input placeholder="Search Businesses" onChange={this.handleTermChange} />
+          <input placeholder="Where?" onChange={this.handleLocationChange} />
         </div>
         <div className="SearchBar-submit">
           <a>Let's Go</a>
