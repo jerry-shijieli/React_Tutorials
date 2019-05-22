06fb27ec6db70feec37bd54962e5892a3e30e50c
sli41 <sli41@ncsu.edu>
2019-05-21 23:54:08 -0700

    Pass information down along SearchResults and Playlist to TrackList

--- jammming/src/components/App/App.js
+++ jammming/src/components/App/App.js
@@ -2,20 +2,33 @@ import React from 'react';
 import './App.css';
 
 import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
 
-function App() {
-  return (
-    <div>
-      <h1>Ja<span class="highlight">mmm</span>ing</h1>
-      <div className="App">
-        <SearchBar />
-        <div className="App-playlist">
-          <!-- Add a SearchResults component -->
-          <!-- Add a Playlist component -->
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [{id: '1', name:'x', artist: 'y', album: 'z'}],
+      playlistName: 'myplaylist',
+      playlistTracks: [{id: '2', name:'a', artist: 'b', album: 'c'}]
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults searchResults={this.state.searchResults} />
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
+};
 
 export default App;
