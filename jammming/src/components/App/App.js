cf5481536fc9f382d44beacdf2866ba0ac0073e7
sli41 <sli41@ncsu.edu>
2019-05-21 21:37:02 -0700

    Add and set SearchBar component

--- jammming/src/components/App/App.js
+++ jammming/src/components/App/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 
-
+import SearchBar from '../SearchBar/SearchBar';
 
 function App() {
   return (
     <div>
       <h1>Ja<span class="highlight">mmm</span>ing</h1>
       <div className="App">
-        <!-- Add a SearchBar component -->
+        <SearchBar />
         <div className="App-playlist">
           <!-- Add a SearchResults component -->
           <!-- Add a Playlist component -->
